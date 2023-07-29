ddac033091c24771f5da676f7425e297079cf314
ganeshkale07 <ggkale07@gmail.com>
2023-07-29 15:41:26 +0530

    Create own custom streams

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import http  from "http";
-import fs from 'fs';
+import fs, { read } from 'fs';
+import { Readable, Writable } from "stream";
 
 const server = http.createServer((req,res)=>{
     //Note - by default on home url "/" browser make one more call fro "Faviocon"  - "/favicon"
@@ -28,15 +29,37 @@ const server = http.createServer((req,res)=>{
     //const writeFile = fs.writeFileSync('dummy.txt',readFile); 
 
     //writing file using streams
-    const readFile = fs.createReadStream('sample.txt');
-    const writeFile = fs.createWriteStream('dummy.txt');
+    //const readFile = fs.createReadStream('sample.txt');
+    //const writeFile = fs.createWriteStream('dummy.txt');
 
     //Event listerner we added for writing file
     //Node js internally invoked "data" event then writing will start
     //invoking happens when chunks of data started coming inside readable CMP
-    readFile.on('data', (chunks) => {
-        writeFile.write(chunks);
+    
+    //readFile.on('data', (chunks) => {
+    //    writeFile.write(chunks);
+    //})
+
+
+    //Custom streams | pipes | error handlings in pip with pipeline
+    
+    /* CREATE YOUR OWN CUSTOM STREAMS
+     const readStreamForFile = new Readable({
+        read(){}
+    });
+    const writeStreamForFile = new Writable({
+        write(s){
+            console.log("Writing", s);
+        }
+    });
+
+    readStreamForFile.on('data', (chunks) =>{
+        const finalString = chunks.toString().toUpperCase();
+        writeStreamForFile.write(finalString);
     })
+
+    readStreamForFile.push('hello World ');
+     */
     
     res.end();
 
